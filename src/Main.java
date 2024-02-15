d7b1295b25d44143e59b3614114e1fbf34eb7fb3
Miguerfi <miguelpintojose00@gmail.com>
2024-02-15 20:37:46 -0300

    feito um programa simples para calcular a média do aluno.

---
+++ src/Main.java
@@ -0,0 +1,29 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        List<Integer> notas = new ArrayList<>();
+        for(int i = 1;i<=4;i++){
+            System.out.println("Digite a nota "+i);
+            int nota = s.nextInt();
+            notas.add(nota);
+        }
+        Integer media = calcularMedia(notas);
+        if(media >= 5) {
+            System.out.println("Aprovado");
+        } else {
+            System.out.println("Reprovado");
+        }
+    }
+
+    public static int calcularMedia(List<Integer> notas){
+        int soma = 0;
+        for (Integer nota : notas) {
+            soma += nota;
+        }
+        return soma/notas.size();// aqui poderia ser feito dividindo exatamente por 4, mas deixar dessa maneira abre espaço para ter um calculo mais dinamico
+    }
+}
